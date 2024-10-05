9d136309553fb1f80dd72966cd2d4885b56e10af
Chris Padwick <cpadwick@hotmail.com>
2024-10-05 15:37:53 -0700

    complete changes for reading cal data from file

--- src/json_test.cpp
+++ src/json_test.cpp
@@ -7,11 +7,18 @@ using json = nlohmann::json;
 int main(void) {
   std::ifstream f("/home/nvidia/code/OrinVisionSystem/calibrationmatrix.json");
   json data = json::parse(f);
+  std::cout << "Matrix: " << std::endl;
   for (int i = 0; i < 3; ++i) {
     for (int j = 0; j < 3; ++j) {
       std::cout << data["matrix"][i][j] << " ";
     }
     std::cout << std::endl;
   }
+
+  std::cout << "Distortion Coeffs:" << std::endl;
+  for (int i = 0; i < 5; ++i) {
+    std::cout << data["disto"][0][i] << ", ";
+  }
+  std::cout << std::endl;
   return 0;
 }
