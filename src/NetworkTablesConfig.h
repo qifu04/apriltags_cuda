7d23696403f893ed244dbc160db32635ffbae324
Max Spier <68516760+Mixmix00@users.noreply.github.com>
2024-10-09 17:57:06 -0700

    make names static

--- src/NetworkTablesConfig.h
+++ src/NetworkTablesConfig.h
@@ -1,4 +1,4 @@
 #include <string>
 
-const std::string table_address = "10.7.66.2";
-const std::string table_name = "/SmartDashboard"
+const std::string TABLE_ADDRESS = "10.7.66.2";
+const std::string TABLE_NAME = "/SmartDashboard";
