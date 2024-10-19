fe6d357c69f831d28f858f3326c9b895b0659e14
Aaron Chung <815466@seq.org>
2024-10-19 15:35:59 -0700

    implement retry for missing camera

--- src/DoubleValueSender.h
+++ src/DoubleValueSender.h
@@ -1,23 +1,24 @@
 #ifndef DOUBLEVALUESENDER_H
 #define DOUBLEVALUESENDER_H
 
-#include "networktables/NetworkTable.h"
-#include "networktables/NetworkTableInstance.h"
-#include "networktables/DoubleTopic.h"
-
 #include <string>
 
-class DoubleValueSender {
-  private:
-    nt::NetworkTableInstance inst_;
-    nt::DoublePublisher publisher_;
-  public:
-    // Constructor declaration
-    DoubleValueSender(std::string key);
+#include "networktables/DoubleTopic.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
 
-    // Method declarations
-    void sendValue(double value);
-    void setDefaultValue(double value);
+class DoubleValueSender {
+ private:
+  nt::NetworkTableInstance inst_;
+  nt::DoublePublisher publisher_;
+
+ public:
+  // Constructor declaration
+  DoubleValueSender(std::string key);
+
+  // Method declarations
+  void sendValue(double value);
+  void setDefaultValue(double value);
 };
 
 #endif
